5ff502585242b4fb798cac85935300e91bde5138
Nirvana Jay <nirvanajay@gmail.com>
2018-12-21 06:17:09 +0800

    minor change.

--- eds-three/js/CTMLoader.js
+++ eds-three/js/CTMLoader.js
@@ -110,7 +110,7 @@ THREE.CTMLoader.prototype.load = function ( url, callback, parameters ) {
 
 				if ( parameters.useWorker ) {
 
-					var worker = parameters.worker || new Worker( 'js/CTMWorker.js' );
+					var worker = parameters.worker || new Worker( './js/CTMWorker.js' );
 
 					worker.onmessage = function ( event ) {
 
